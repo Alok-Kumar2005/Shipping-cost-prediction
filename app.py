bf644cd3b928a0454920d4948fb8158fbd026c13
Alok-Kumar2005 <ay747283@gmail.com>
2025-01-12 23:32:51 +0530

    code updated

--- app.py
+++ app.py
@@ -1,3 +1,11 @@
 from shipment.logger import logging
+from shipment.exception import shippingException
+import sys
 
 logging.info("Testing logging setup")
+
+try:
+    a = 2 + '3'
+    print(a)
+except Exception as e:
+    raise shippingException(e, sys) from e
